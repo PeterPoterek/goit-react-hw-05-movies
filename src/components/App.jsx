c29af0d9bd496f48a7219b60e7efe26e2f546c62
Piotr Poterek <peterpoterek0@gmail.com>
2024-02-08 17:55:09 +0100

    react router setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,19 @@
 import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 
+import Home from './Home/Home.jsx';
 import Navbar from './Navbar/Navbar.jsx';
+import Movies from './Movies/Movies.jsx';
 
 export const App = () => {
-  useEffect(() => {
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTQ4MjQ0ZDExZTA2ZWZmYWI5YTlmNzcyYjNkNTZkNSIsInN1YiI6IjY1YzNhMDVlMmZlMmZhMDE4NDJhODNlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ScnllNORtvUmvsjjjXP4A6vybDJMgX-vlcvg-Jewp1w',
-      },
-    };
-
-    axios
-      .get(
-        'https://api.themoviedb.org/3/trending/all/day?language=en-US',
-        options
-      )
-      .then(response => console.log(response.data))
-      .catch(err => console.error(err));
-  }, []);
-
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
