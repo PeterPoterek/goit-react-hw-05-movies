33448ed8bf5be845d937b2d0b0bd744efe64c0eb
Piotr Poterek <peterpoterek0@gmail.com>
2024-02-08 18:52:29 +0100

    Lazy, Suspens

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,30 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './Home/Home.jsx';
 import Navbar from './Navbar/Navbar.jsx';
-import Movies from './Movies/Movies.jsx';
-import MovieDetails from './MovieDetails/MovieDetails.jsx';
-import Cast from './Cast/Cast.jsx';
-import Reviews from './Reviews/Reviews.jsx';
+import LoadingSpinner from './LoadingSpinner/LoadingSpinner.jsx';
+import NotFound from './NotFound/NotFound.jsx';
+
+const Home = lazy(() => import('./Home/Home.jsx'));
+const Movies = lazy(() => import('./Movies/Movies.jsx'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails.jsx'));
+const Cast = lazy(() => import('./Cast/Cast.jsx'));
+const Reviews = lazy(() => import('./Reviews/Reviews.jsx'));
 
 export const App = () => {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />} />
-        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
