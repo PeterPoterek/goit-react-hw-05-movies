5dfd983f5b9bf18aa77b33c121f34b94f427b463
Piotr Poterek <peterpoterek0@gmail.com>
2024-02-09 19:15:01 +0100

    Movies search

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,52 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Movies = () => {
+  const [newMovieSearch, setNewMovieSearch] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const handleMovieSearch = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/search/movie',
+        {
+          params: {
+            include_adult: false,
+            language: 'en-US',
+            page: 1,
+            query: newMovieSearch,
+          },
+          headers: {
+            accept: 'application/json',
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWM4YjU4MjhiNGVjNGMwZjU5YzFkZTVlMmQ1ZTNiOCIsInN1YiI6IjY1YzNhMDVlMmZlMmZhMDE4NDJhODNlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V9SAFWmxQkdTT5KUPdr_PSuh1d-FJ7WmxFeruZ9_Y5Q',
+          },
+        }
+      );
+
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h2>Movies</h2>
-      <Link to={'/movies/1'}>Movie - 1</Link>
-      <Link to={'/movies/2'}>Movie - 2</Link>
+      <input
+        onChange={e => setNewMovieSearch(e.target.value)}
+        type="text"
+        value={newMovieSearch}
+      />
+      <button onClick={handleMovieSearch}>Search</button>
+
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
